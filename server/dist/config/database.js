2677ab4c192541d3844417d3fff39bba86528030
Rohit Shahi <rohitshahi581@gmail.com>
2025-07-05 00:33:05 +0530

    Debug

--- server/dist/config/database.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-    errorformat: 'pretty',
-});
-export default prisma;
